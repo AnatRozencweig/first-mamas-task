e3aad83f71e651111cf5b5dc38c941200e1ec150
AnatRozencweig <anatrozencweig@gmail.com>
2024-05-06 00:20:11 +0300

    in algotithmic p4 reformated and added exceptions

--- algorithmic/p4.py
+++ algorithmic/p4.py
@@ -6,25 +6,26 @@ def input_numbers_series() -> list:
     input_num = 0
     input_number_list = []
     while input_num != -1:
-        input_num = float(input("enter number (to finish enter -1): "))
+        try:
+            input_num = float(input("enter number (to finish enter -1): "))
+        except ValueError:
+            print("Input is not a number")
         input_number_list.append(input_num)
     input_number_list.remove(-1)
     return input_number_list
 
 
 def calc_list_average(numbers_list: list) -> float:
-    sum = 0
     numbers_amount = len(numbers_list)
-    for num in numbers_list: sum += num
-    return sum / numbers_amount
+    lst_sum = sum(numbers_list)
+    try:
+        return lst_sum / numbers_amount
+    except ZeroDivisionError:
+        print("entered only -1, list is empty")
 
 
 def calc_positive_numbers_amount(numbers_list: list) -> int:
-    positive_numbers_amount = 0
-    for num in numbers_list:
-        if num > 0:
-            positive_numbers_amount += 1
-    return positive_numbers_amount
+    return len([num for num in numbers_list if num > 0])
 
 
 def show_chosen_numbers_series_details() -> None:
@@ -34,8 +35,8 @@ def show_chosen_numbers_series_details() -> None:
         print(f"The amount of positive numbers in the series: {calc_positive_numbers_amount(input_number_list)}")
         input_number_list.sort()
         print(f"The sorted numbers series: {input_number_list}")
-    except:
-        print("it seems like you didn't entered numbers yet")
+    except BaseException as e:
+        print(f"Failed to run: got exception {e}")
 
 
 def show_chosen_numbers_graph(numbers_list: list) -> None:
@@ -44,4 +45,4 @@ def show_chosen_numbers_graph(numbers_list: list) -> None:
     plt.show()
 
 
-show_chosen_numbers_graph(input_numbers_series())
+show_chosen_numbers_series_details()
