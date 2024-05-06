3b1bfd02906c72742a5465c16fd61f3defd7073a
AnatRozencweig <anatrozencweig@gmail.com>
2024-05-06 11:56:21 +0300

    in algotithmic p2 fixed find_all_pythagorean_triples_that_make_sum to find all options and not just one

--- algorithmic/p2.py
+++ algorithmic/p2.py
@@ -1,17 +1,15 @@
-def find_all_pythagorean_triples_until_max(max_num: int) -> list:
-    """
-    this function find all pythagorean triples whose three numbers are less than the maximum
-    """
+def find_all_pythagorean_triples_that_make_sum(triplet_sum: int) -> list:
     pythagorean_triples_list = []
-    n = 2
-    while n * 3 < max_num and n * 4 < max_num and n * 5 < max_num:
-        pythagorean_triples_list.append((3 * n, 4 * n, 5 * n))
-        n += 1
+    for a in range(1, triplet_sum):
+        for b in range(a, triplet_sum):
+            c = triplet_sum - a - b
+            if c > 0 and c ** 2 == a ** 2 + b ** 2:
+                pythagorean_triples_list.append((a, b, c))
     return pythagorean_triples_list
 
 
 def pythagorean_triplet_by_sum(triplet_sum: int) -> None:
-    pythagorean_triples_list = find_all_pythagorean_triples_until_max(triplet_sum)
+    pythagorean_triples_list = find_all_pythagorean_triples_that_make_sum(triplet_sum)
     for triple in pythagorean_triples_list:
         if sum(triple) == triplet_sum:
             print(f"{triple[0]} < {triple[1]} < {triple[2]}")
