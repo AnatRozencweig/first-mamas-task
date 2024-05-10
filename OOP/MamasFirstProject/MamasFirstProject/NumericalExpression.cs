5deb3b964aa8572ab875c8957801dcd5f538e579
AnatRozencweig <anatrozencweig@gmail.com>
2024-05-10 17:48:26 +0300

    in OOP in FirstMamasProject added to class NumericalExpression GetValue and SumLetters

---
+++ OOP/MamasFirstProject/MamasFirstProject/NumericalExpression.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MamasFirstProject
+{
+    class NumericalExpression
+    {
+        public long Number;
+        private string[] Ones = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        private string[] Teens = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        private string[] Tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        private string[] Thousands = { "", "Thousand", "Million", "Billion", "Trillion" };
+        public NumericalExpression(long number)
+        {
+            Number = number;
+        }
+        public string ConvertHundreds(long number)
+        {
+            string words = "";
+
+            if (number >= 100)
+            {
+                words += Ones[number / 100] + " Hundred ";
+                number %= 100;
+            }
+
+            if (number >= 10 && number <= 19)
+            {
+                words += Teens[number % 10] + " ";
+            }
+            else
+            {
+                words += Tens[number / 10] + " ";
+                words += Ones[number % 10] + " ";
+            }
+
+            return words;
+        }
+        public string ConvertNumberToWords()
+        {
+            long number = Number;
+            if (number == 0)
+                return "Zero";
+
+            string words = "";
+
+            for (int i = 0; number > 0; i++)
+            {
+                if (number % 1000 != 0)
+                    words = ConvertHundreds(number % 1000) + Thousands[i] + " " + words;
+
+                number /= 1000;
+            }
+
+            return words.Trim();
+        }
+
+
+        public override string ToString()
+        {
+            return ConvertNumberToWords();
+        }
+        public long GetValue()
+        {
+            return Number;
+        }
+        public int SumLetters()
+        {
+            string numberInWords = ToString();
+            string trim = numberInWords.Replace(" ", "");
+            return trim.Length;
+        }
+    }
+}
--- OOP/MamasFirstProject/MamasFirstProject/Program.cs
+++ OOP/MamasFirstProject/MamasFirstProject/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MamasFirstProject
 {
@@ -10,16 +7,9 @@ namespace MamasFirstProject
     {
         static void Main(string[] args)
         {
-            Node d = new Node(5);
-            Node c = new Node(9, d);
-            Node b = new Node(1, c);
-            Node a = new Node(2, b);
-            LinkedList abc = new LinkedList(a);
-            abc.PrintLinkedList();
-            Console.WriteLine();
-            Console.WriteLine(abc.GetMaxNode().Value);
-            Console.WriteLine();
-            abc.PrintLinkedList();
+            NumericalExpression six = new NumericalExpression(3);
+            
+            Console.WriteLine(six.SumLetters());
         }
     }
 }
