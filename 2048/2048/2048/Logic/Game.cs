7f663ecef3797d2effdf6c2eb2b6237367364887
AnatRozencweig <anatrozencweig@gmail.com>
2024-05-14 19:36:53 +0300

    in 2048 in FirstMamasProject added class Game

---
+++ 2048/2048/2048/Logic/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048.Logic
+{
+    class Game
+    {
+        public Board GameBoard { get; set; }
+        public GameStatus GameStatus { get; set; }
+        public int Points { get; protected set; }
+        public Game(Board board)
+        {
+            GameBoard = board;
+            Points = 0;
+        }
+
+
+    }
+}
