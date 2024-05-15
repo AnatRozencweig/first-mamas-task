01599affc9a903d4af2f01be530fed365e425155
AnatRozencweig <anatrozencweig@gmail.com>
2024-05-15 13:16:18 +0300

    in 2048 in FirstMamasProject added class ConsoleGame and function GetDirection

---
+++ 2048/2048/2048/UI/ConsoleGame.cs
@@ -0,0 +1,36 @@
+﻿using _2048.Logic;
+using System;
+
+namespace _2048.UI
+{
+    class ConsoleGame
+    {
+        public Board GameBoard { get; set; }
+        public ConsoleGame(Board board)
+        {
+            GameBoard = board;
+        }
+        public Direction GetDirection()
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            switch (char.ToUpper(keyInfo.KeyChar))
+            {
+                case 'W':
+                    return Direction.Up;
+                case 'S':
+                    return Direction.Down;
+                case 'A':
+                    return Direction.Left;
+                case 'D':
+                    return Direction.Right;
+                default:
+                    throw new InvalidOperationException("You press invalid key"); ;
+            }
+        }
+        public void PrintBoard()
+        {
+
+        }
+    }
+}
